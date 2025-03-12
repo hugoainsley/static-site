de164cfc40b5e29ab1dc7701b486f54e69718b4f
Hugo Ainsley <hugoainsley@gmail.com>
2025-03-12 22:38:42 +0100

    Add Link Extraction

--- src/test_splitnode.py
+++ src/test_splitnode.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from splitnode import split_nodes_delimiter
+from splitnode import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNode(unittest.TestCase):
     def test_split_node_code(self):
@@ -17,3 +17,38 @@ class TestSplitNode(unittest.TestCase):
         node = TextNode("This is _italic_", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
         self.assertEqual(new_nodes, [TextNode("This is ", TextType.TEXT, None), TextNode("italic", TextType.ITALIC, None), TextNode("", TextType.TEXT, None)])
+
+    def test_split_images(self):
+        node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode(" with text that follows", TextType.TEXT),
+            ],
+            new_nodes,
+        )   
